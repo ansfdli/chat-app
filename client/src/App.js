308048799ebe276adcc0feac2aba6fa516d2bf4a
ansfdli <anasfadli@outlook.com>
2022-07-28 17:29:55 +0700

    add css & finishing

--- client/src/App.js
+++ client/src/App.js
@@ -8,15 +8,19 @@ const socket = io.connect("http://localhost:3001");
 function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {
     if (username !== "" && room !==""){
       socket.emit("join_room", room);
+      setShowChat(true);
     }
   };
 
   return (
     <div className="App">
+      {!showChat ? (
+      <div className="joinChatContainer">
       <h3>Join A Chat</h3>
       <input type="text" placeholder="John..." onChange={(event) => {
         setUsername(event.target.value);
@@ -25,8 +29,12 @@ function App() {
         setRoom(event.target.value);
       }}/>
       <button onClick={joinRoom}>Join A Room</button>
+    </div>
+      )
+    : (
 
       <Chat socket={socket} username={username} room={room} />
+    )}
     </div>
   );
 }
